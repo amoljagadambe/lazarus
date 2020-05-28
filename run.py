41fa8fe3d4e21612cd95228d9708db458577bb0e
amoljagadambe <amol.jagdambe@gmail.com>
2020-05-28 13:02:39 +0530

    flaskenv file to reslove the FLASK_APP issue at start

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from application import app
-from dotenv import load_dotenv
-import os
-
-env_path = '{0}\\.env'.format(os.getcwd())
-load_dotenv(dotenv_path=env_path)
 
 if __name__ == '__main__':
     app.run()
+
+# TODO: Add support for gunicorn to run the code for production server
