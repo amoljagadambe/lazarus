1ae0df2f007100e2b1842b7c7b17e06c33ddbbaa
amoljagadambe <amol.jagdambe@gmail.com>
2020-05-28 22:47:07 +0530

    changes in scheduler calling

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,20 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from application.resources.models.model_controller import sensor
+
 from flask import Flask
 from flask_cors import CORS
-from flask_restx import Api
+from flask_restplus import Api
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app, title='Lazarus Pit', version='1.0', description='Created by Amol Jagadambe',
+          default='Lazarus API', default_label='Web Services', doc='/swagger-ui.html/',
+          validate=True, contact_url='http://www.gais.co.in', contact_email='amol.jagadambe@gmail.com',
+          license='Apache 2.0', license_url='http://www.apache.org/licenses/LICENSE-2.0',
+          )
+
+from application.resources import home
+from application.resources.models.model_controller import sensor
 
 """
 init BackgroundScheduler job daemon 
@@ -14,14 +26,3 @@ sched_data = BackgroundScheduler(daemon=True)
 # in your case you could change seconds to hours
 sched_data.add_job(sensor, 'interval', seconds=5)
 sched_data.start()
-
-app = Flask(__name__)
-CORS(app)
-
-api = Api(app, title='Lazarus Pit', version='1.0', description='Created by Amol Jagadambe',
-          default='Lazarus API', default_label='Web Services', doc='/swagger-ui.html/',
-          validate=False, contact_url='http://www.gais.co.in', contact_email='amol.jagadambe@gmail.com',
-          license='Apache 2.0', license_url='http://www.apache.org/licenses/LICENSE-2.0',
-          )
-
-from application.resources import home
--- application/resources/db_store/db_connection.py
+++ application/resources/db_store/db_connection.py
@@ -1,5 +1,6 @@
 from application.resources.db_store.utils import config
-from mysql.connector import connection
+from mysql.connector import errorcode
+import mysql.connector as connection
 from application import app
 
 db_values = config['mysqld']
@@ -11,7 +12,20 @@ class MySQLDBConnection:
     def __init__(self):
         app.logger.info('MySQLDBConnection initiated....')
         # TODO : Add proper Logging with logger module
-        self.ds = connection.MySQLConnection(**db_values)
+        try:
+            self.ds = connection.connect(**db_values)
+        except connection.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                app.logger.error("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                app.logger.error("Database does not exist")
+            else:
+                app.logger.error(err)
 
-    def get_data_source(self):
-        return self.ds
+    def get_cursor(self):
+        return self.ds.cursor()
+
+    def findBy(self, sql):
+        self.cur = self.get_cursor()
+        self.cur.execute(sql)
+        return self.cur.fetchall()
