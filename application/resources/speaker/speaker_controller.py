c2b922b156e7c1b4d58499c12ef299926b5c3963
amoljagadambe <amol.jagdambe@gmail.com>
2020-06-02 17:38:33 +0530

    call for verification is updated

--- application/resources/speaker/speaker_controller.py
+++ application/resources/speaker/speaker_controller.py
@@ -1,7 +1,9 @@
-from application.resources.db_store.db_connection import MySQLDBConnection
+from application.resources.speaker.speaker_verify import verify_speaker
 from application.resources.speaker.user_input import user_fields
+from flask_restx import Resource
+from flask import request
 import json
-from flask_restplus import Resource
+
 
 from application import api
 
@@ -11,13 +13,11 @@ speakersController = api.namespace('speaker', description='Speakers Controller')
 @speakersController.route('/verify')
 class SpeakersController(Resource):
 
-    # @api.doc('example_field')
-    # @api.expect(user_fields, validate=False)
+    @api.expect(user_fields, validate=False)
     def post(self):
-        conn = MySQLDBConnection()
-        query = "SELECT id FROM tb_ag_user"
-        data = conn.findBy(query)
-        for u_id in data:
-            print(type(u_id[0]))
-            print(u_id[0])
-        return json.dumps(data)
+        json_data = request.get_json(force=True)
+        file_path = json_data['audio_location']
+        user_id = json_data['user_id']
+        print(file_path)
+        verfied_status = verify_speaker(file_path, user_id)
+        return json.dumps(verfied_status)
--- application/resources/speaker/speaker_verify.py
+++ application/resources/speaker/speaker_verify.py
@@ -1,4 +1,7 @@
+from application.resources.models.feature_extraction import extract_features
 import _pickle as cPickle
+from scipy.io.wavfile import read
+import numpy as np
 import os
 
 # path to training data
@@ -14,3 +17,24 @@ gmm_files = [os.path.join(FULL_PATH, fname) for fname in
 models = [cPickle.load(open(fname, 'rb')) for fname in gmm_files]
 speakers = [fname.split("/")[-1].split(".gmm")[0] for fname
             in gmm_files]
+
+
+def verify_speaker(path: str, user_id: int):
+
+    sr, audio = read(path)
+    vector = extract_features(audio, sr)
+    print(vector)
+    log_likelihood = np.zeros(len(models))
+    for i in range(len(models)):
+        gmm = models[i]  # checking with each model one by one
+        scores = np.array(gmm.score(vector))
+        log_likelihood[i] = scores.sum()
+
+    deteced_id = np.argmax(log_likelihood)
+    # print("\tdetected as - ", speakers[deteced_id])
+
+    if speakers[deteced_id] == user_id:
+        return "Speaker Verified"
+
+    else:
+        return "Speaker actual id is {}".format(speakers[deteced_id])
